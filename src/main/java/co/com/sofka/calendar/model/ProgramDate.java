a438519c39ebd2d28ea7f81775d44e503f77c923
David Zuluaga <zuluroa@gmail.com>
2021-12-10 14:39:17 -0500

    Se hace una subscripcion del servicio reactivo y hacemos un test el cual espera 13 elementos

--- src/main/java/co/com/sofka/calendar/model/ProgramDate.java
+++ src/main/java/co/com/sofka/calendar/model/ProgramDate.java
@@ -19,4 +19,12 @@ public final class ProgramDate {
     public String getCategoryName() {
         return categoryName;
     }
+
+    @Override
+    public String toString() {
+        return "ProgramDate{" +
+                "categoryName='" + categoryName + '\'' +
+                ", date=" + date +
+                '}';
+    }
 }
--- src/test/java/co/com/sofka/calendar/services/SchedulerServiceTest.java
+++ src/test/java/co/com/sofka/calendar/services/SchedulerServiceTest.java
@@ -13,7 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -39,9 +41,12 @@ class SchedulerServiceTest {
 
         Mockito.when(repository.findById(programId)).thenReturn(Mono.just(program));
         //TODO: hacer una subscripción de el servicio reactivo
-        List<ProgramDate> response = schedulerService.generateCalendar(programId, startDate);
-
-        Assertions.assertEquals(13, response.size());//TODO: hacer de otro modo
+        Flux<ProgramDate> response = schedulerService.generateCalendar(programId, startDate);
+        response.subscribe();
+        StepVerifier.create(response)
+                .expectNextCount(13)
+                .verifyComplete();
+        //Assertions.assertEquals(13, response.size());//TODO: hacer de otro modo
         Assertions.assertEquals(getSnapResult(), new Gson().toJson(response));//TODO: hacer de otro modo
         Mockito.verify(repository).findById(programId);
     }
